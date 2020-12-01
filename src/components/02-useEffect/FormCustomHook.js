21c5b296ed0b309ccd4a9453eacb3e77402fdd95
chriszdeux <chriszdeux@gmail.com>
2020-12-01 13:34:32 -0800

    Form with custom hook

--- src/components/02-useEffect/FormCustomHook.js
+++ src/components/02-useEffect/FormCustomHook.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from '../../hook/useForm';
 import '../01-useState/counter.css';
 export const FormCustomHook = () => {
   
-  const [inputValue, handleInputChange] = useForm()
-  const { name, email } = inputValue;
+  const [inputValue, handleInputChange, handleInputSubmit] = useForm()
+  const { name, email, password } = inputValue;
+
+  useEffect(() => {
+    console.log('name changed')
+  }, [ name ])
   return (
-    <>
+    
+    
+    <form onSubmit={ handleInputSubmit }>
       <h1>Form Custom Hooks</h1>
       <hr/>
-
-      <div className="form-group">
-        <input type="text" className="form-control" name="name" value={ name }onChange={ handleInputChange } placeholder="name"/>
-      </div>
-      <div className="form-group">
-        <input type="text" className="form-control" name="email" value={ email }onChange={ handleInputChange } placeholder="email"/>
-      </div>
-    </>
+        <div className="form-group">
+          <input type="text" className="form-control" name="name" value={ name }onChange={ handleInputChange } placeholder="name"/>
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control" name="email" value={ email }onChange={ handleInputChange } placeholder="email"/>
+        </div>
+        <div className="form-group">
+          <input type="password" className="form-control" name="password" value={ password }onChange={ handleInputChange } placeholder="*****"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Save</button>
+      </form>
+    
   )
 }
--- src/hook/useForm.js
+++ src/hook/useForm.js
@@ -4,14 +4,23 @@ export const useForm = () => {
   const [inputValue, setInputValue] = useState( {
     name: '',
     email: '',
+    password: '',
   });
 
   const handleInputChange = ( { target } ) => {
     setInputValue({
       ...inputValue,
+      // remember to compute the nave with the value
       [target.name]:target.value,
     })
   }
 
-  return [ inputValue, handleInputChange]
+  const handleInputSubmit = (event) => {
+    event.preventDefault();
+    console.log( inputValue )
+    // return event;
+    // debugger
+  }
+
+  return [ inputValue, handleInputChange, handleInputSubmit]
 }
