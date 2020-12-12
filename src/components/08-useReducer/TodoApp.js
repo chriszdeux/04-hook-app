7ff2f54c6302454f9822dcf7249a3063470e6d9a
chriszdeux <chriszdeux@gmail.com>
2020-12-12 11:40:07 -0800

    done with todo app

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,17 +1,13 @@
 import React, { useEffect, useReducer } from 'react';
-import { useForm } from '../../hook/useForm';
 import './todo-style.css';
+import { TodoForm } from './AddTodo';
+import { TodoList } from './TodoList';
 import { myReducer } from './todoReducer';
 
 
 const init = () => {
 
   return JSON.parse( localStorage.getItem('todos') ) || []
-  // return [{
-  //   id: new Date().getTime(),
-  //   description: 'Learn React Reduce',
-  //   done: false
-  // }]
 }
 
 
@@ -19,42 +15,9 @@ export const TodoApp = () => {
 
   const [ todos, dispatch ] = useReducer(myReducer, [], init);
 
-  const [ { description }, handleInputChange, resetForm ] = useForm({
-    description: ''
-  });
-  console.log(description);
-
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
-
-
-  const handleSubmit = ( event ) => {
-    
-    if (description.trim().length <= 1) {
-      return 
-    }
-
-    event.preventDefault();
-    console.log('new task');
-
-
-    const newAdd = {
-      id: new Date().getTime(),
-      description: description,
-      done: false
-    };
-
-    const addAction = {
-      type: 'add',
-      payload: newAdd
-    };
-
-    dispatch(addAction);
-    resetForm();
-
-
-  }
 
 
   const handleDelete = (todoId) => {
@@ -70,6 +33,22 @@ export const TodoApp = () => {
   }
 
 
+  const handleToggle = ( todoId ) => {
+    const actionToggle = {
+      type: 'toggle',
+      payload: todoId
+    }
+
+    dispatch(actionToggle)
+  }
+
+  const handleAddTodo = ( newTodo ) => {
+    dispatch({
+      type: 'add',
+      payload: newTodo,
+    })
+  }
+
   return (
     <>
       <h1>TodoApp: { todos.length }</h1>
@@ -77,38 +56,16 @@ export const TodoApp = () => {
 
         <div className="row">
           <div className="col-7">
-            <ul className="list-group list-group-flush">
-            {
-              todos.map( (list, index) => (
-                <li key={ list.id } className="list-group-item">
-                  <p className="text-center" >
-                    {index + 1}. {list.description}  
-                  </p>
-                  <button className="btn btn-danger"
-                  onClick={ () => {
-                    handleDelete(list.id)
-                  } }
-                  >Delete</button>
-                </li>
-              ))
-            }
-            </ul>
+            < TodoList 
+            todos={ todos }
+            handleDelete={ handleDelete }
+            handleToggle={ handleToggle }/>
           </div>
           
           <div className="col-5">
-            <h4>Add Todo</h4>
-            <hr/>
-            <form onSubmit={ handleSubmit }>
-              <input type="text" 
-              className="form-control"
-              placeholder="add todo"
-              autoComplete="off"
-              name="description"
-              value={ description }
-              onChange={ handleInputChange }
-              />
-            <button className="btn btn-outline-primary mt-1 btn-block">Add</button>
-            </form>
+            <TodoForm 
+            handleAddTodo={ handleAddTodo }
+            />
           </div>
         </div>
     </>
